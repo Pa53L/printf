98ab4e715ee28d97983c1f0c865ce7e14a6c286f
Yoda Shawn <yshawn@at-n3.21-school.ru>
2019-12-20 22:06:21 +0300

    outdi

--- s_SRC/out_di.c
+++ s_SRC/out_di.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   out_di.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshawn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/20 20:39:02 by yshawn            #+#    #+#             */
+/*   Updated: 2019/12/20 22:05:49 by yshawn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../h_HEAD/header.h"
 
@@ -8,79 +19,79 @@ size_t	out_di(st_format_spec *spec, int ival)
 
 	tmp_len = 0;
 	len = ft_numlen(ival, 10);
+
+	/* какие-то условия */
 	if (spec->minus)
 		spec->zero = 0;
+	if (spec->plus)
+		spec->space = 0;
+
+	/* условия потяжелее */
 	if (spec->accuracy)
 	{
 		if (spec->accuracy > len)
 			spec->accuracy = spec->accuracy - len;
 		else
 			spec->accuracy = 0;
+		if (spec->accuracy == 0)
+			spec->space = 0;
+		if (spec->accuracy > 0)
+			spec->zero = 0;
 	}
 	if(spec->width)
 	{
+		if (spec->plus)
+			len++;
 		if(spec->width > len + spec->accuracy)
 			spec->width = spec->width - spec->accuracy - len;
 		else
 			spec->width = 0;
-		if (spec->width > 1)
-			spec->space = 0;
-	}
-	if (spec->plus)
-	{
-		spec->space = 0;
-		if (spec->width > 0)
+		if (spec->space)
 			spec->width--;
 	}
-	tmp_len = spec->width + spec->accuracy;
+	tmp_len = spec->width + spec->accuracy + spec->space;
 
+	printf("\n");
+	printf("tmp is + len is: %lu\n", tmp_len + len);
+	printf("\n");
 	/* непосредственно, вывод */
-
+	
 	if (spec->minus == 0)
 	{
 		while (spec->width > 0)
 		{
-			write(1, " ", 1);
+			spec->zero ? write(1, "0", 1) : write(1, " ", 1);
 			spec->width--;
 		}
-		if (spec->plus)
-		{
-			tmp_len++;
+		if (spec->plus && ival >= 0)
 			write(1, "+", 1);
-		}
-		while (spec->accuracy)
+		if (spec->space && ival >= 0)
+			write(1, " ", 1);
+		while (spec->accuracy > 0)
 		{
 			write(1, "0", 1);
 			spec->accuracy--;
-		}
-		if (spec->space)
-		{
-			write(1, " ", 1);
-			tmp_len++;
 		}
 		ft_putnbr(ival);
 	}
 	if (spec->minus == 1)
 	{
-		if (spec->plus)
+		if (spec->plus && ival >= 0)
 		{
 			tmp_len++;
 			write(1, "+", 1);
 		}
-		while (spec->accuracy)
+		if (spec->space && ival >= 0)
+			write(1, " ", 1);
+		while (spec->accuracy > 0)
 		{
 			write(1, "0", 1);
 			spec->accuracy--;
 		}
-		if (spec->space && spec->width <= 0)
-		{
-			write(1, " ", 1);
-			tmp_len++;
-		}
 		ft_putnbr(ival);
 		while (spec->width > 0)
 		{
-			write(1, " ", 1);
+			(spec->zero == 1) ? write(1, "0", 1) : write(1, " ", 1);
 			spec->width--;
 		}
 	}
